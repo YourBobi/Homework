40a4b2d46e931b5a6dc7377f055cb145ad69f08e
YourBobi <still.dogly@gmail.com>
2021-10-30 14:32:47 +0300

    Add method for creating html with news

--- SelivonchykKolya/taskmanager/news/views.py
+++ SelivonchykKolya/taskmanager/news/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from RR import MyParser, create_keys, CreateFile
+from bs4 import BeautifulSoup
 
 def index(request):
+    search_url = request.GET.get('search')
+    if search_url:
+        rss = MyParser(search_url, keys=create_keys())
+        with open('news/templates/news/news.html', 'w', encoding="UTF-8") as file:
+            s = """{% extends 'news/index.html' %}
+            {% block content %}""" + CreateFile(rss).create_sample_html() + "{% endblock %}"
+            file.write(s)
+
+        return render(request, 'news/news.html', {'rss': rss})
+
     return render(request, 'news/index.html')
 
 
