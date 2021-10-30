87d0128169ea6b1213d66f097761f5c3178717fb
YourBobi <still.dogly@gmail.com>
2021-10-30 20:28:28 +0300

    Add sorting for data in RssReader objects

--- SelivonchykKolya/taskmanager/news/views.py
+++ SelivonchykKolya/taskmanager/news/views.py
@@ -3,16 +3,21 @@ from django.http import HttpResponse
 from RR import MyParser, create_keys, CreateFile
 from bs4 import BeautifulSoup
 
+
 def index(request):
     search_url = request.GET.get('search')
     if search_url:
-        rss = MyParser(search_url, keys=create_keys())
-        with open('news/templates/news/news.html', 'w', encoding="UTF-8") as file:
-            s = """{% extends 'news/index.html' %}
-            {% block content %}""" + CreateFile(rss).create_sample_html() + "{% endblock %}"
-            file.write(s)
+        try:
+            rss = MyParser(search_url, keys=create_keys())
+            rss.sort()
+            with open('news/templates/news/news.html', 'w', encoding="UTF-8") as file:
+                s = """{% extends 'news/index.html' %}
+                {% block content %}""" + CreateFile(rss).create_sample_html() + "{% endblock %}"
+                file.write(s)
 
-        return render(request, 'news/news.html', {'rss': rss})
+            return render(request, 'news/news.html', {'rss': rss})
+        except:
+            return render(request, 'news/index.html')
 
     return render(request, 'news/index.html')
 
