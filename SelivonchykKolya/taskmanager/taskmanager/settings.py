69895e7effc836d130bf9956d74dd0bafe48646b
YourBobi <still.dogly@gmail.com>
2021-10-30 14:34:36 +0300

    html and other

--- SelivonchykKolya/taskmanager/taskmanager/settings.py
+++ SelivonchykKolya/taskmanager/taskmanager/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j@o7ba+#v9%b6a@6=9%4uq7xd^xyt5ed9qk9&xgtay5w^ebbke'
+SECRET_KEY = 'django-insecure-j@o7ba+#v9%b6a@6=9%4uq7xd^xyt5ed9qgftgtgk9&xgtay5w^ebbke'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
